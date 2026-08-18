20af8b8ffb8a5b568d6b76d93ab83de0a7bb8965
Hasib Mallick <hasibmallick947933@gmail.com>
2026-08-18 17:08:26 +0530

    Import Question model in API test handler

--- pages/api/tests/[id].js
+++ pages/api/tests/[id].js
@@ -1,5 +1,6 @@
 import { dbConnect } from '../../../lib/db';
 import Test from '../../../models/Test';
+import Question from '../../../models/Question';
 import { getTokenFromRequest, verifyToken } from '../../../lib/auth';
 
 export default async function handler(req, res) {
